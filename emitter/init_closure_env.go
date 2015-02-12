8e4eac941a6f2ac64e2b126cabc807ef1ddc0949
Rustam <rfatkullin@yandex.ru>
2015-02-12 20:45:44 +0300

    Подготовка.

--- emitter/init_closure_env.go
+++ emitter/init_closure_env.go
@@ -8,32 +8,23 @@ import (
 	"BMSTU-Refal-Compiler/syntax"
 )
 
-func (f *Data) searchVarsInTerms(terms []*syntax.Term, envVarMap map[string]syntax.ScopeVar) {
-
-	for _, term := range terms {
-
-		if term.TermTag == syntax.VAR {
-			if _, ok := envVarMap[term.Value.Name]; !ok {
-				envVarMap[term.Value.Name] = syntax.ScopeVar{Number: len(envVarMap), VarType: term.VarType}
-				fmt.Printf("Unknown var: %s\n", term.Value.Name)
-			}
-		}
-	}
-}
-
 func (f *Data) setEnvNestedFuncs(funcs map[string]*syntax.Function) {
 
 	for _, currFunc := range funcs {
-		if currFunc.EnvVarMap == nil {
-			currFunc.EnvVarMap = make(map[string]syntax.ScopeVar, 8)
+
+		currFunc.EnvVarMap = make(map[string]syntax.ScopeVar, 8)
+
+		for s := &currFunc.Params; s != nil; s = s.Parent {
+			if s.VarMap != nil {
+				for varName, varInfo := range s.VarMap {
+					currFunc.EnvVarMap[varName] = syntax.ScopeVar{Number: len(currFunc.EnvVarMap), VarType: varInfo.VarType}
+				}
+			}
 		}
 
-		for _, s := range currFunc.Sentences {
-			f.searchVarsInTerms(s.Pattern.Terms, currFunc.EnvVarMap)
-
-			for _, a := range s.Actions {
-				f.searchVarsInTerms(a.Terms, currFunc.EnvVarMap)
-			}
+		fmt.Printf("Function %s\n", currFunc.FuncName)
+		for varName, _ := range currFunc.EnvVarMap {
+			fmt.Printf("\t%s\n", varName)
 		}
 	}
 }
